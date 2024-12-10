8fb6de4adc5a2f256d562447eed6be3dc19893e6
filomenazong <filomenazong@gmail.com>
2024-12-10 21:08:47 +0100

    nie ładnie wygląda usert tabela

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,41 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="container">
-                <div class="shadow-lg rounded-lg overflow-hidden mx-4">
-                    <table class="w-full table-fixed">
-                        <thead>
-                            <tr>
-                                <th class="w-1/7 py-4 px-6 text-left text-gray-600 font-bold uppercase">{{ __('users.attributes.id') }}</th>
-                                <th class="w-1/7 py-4 px-6 text-left text-gray-600 font-bold uppercase">{{ __('users.attributes.name') }}</th>
-                                <th class="w-1/7 py-4 px-6 text-left text-gray-600 font-bold uppercase">{{ __('users.attributes.email') }}</th>
-                                <th class="w-1/7 py-4 px-6 text-left text-gray-600 font-bold uppercase">{{ __('users.attributes.email_verified_at') }}</th>
-                                <th class="w-1/7 py-4 px-6 text-left text-gray-600 font-bold uppercase">{{ __('users.attributes.created_at') }}</th>
-                                <th class="w-1/7 py-4 px-6 text-left text-gray-600 font-bold uppercase">{{ __('users.attributes.roles') }}</th>
-                            </tr>
-                        </thead>
-
-                            <tbody class="bg-white">
-                                @foreach ($users as $user)
-                                <tr>
-                                    <td class="py-4 px-6 border-b border-gray-200">{{$user->id}} </td>
-                                    <td class="py-4 px-6 border-b border-gray-200">{{$user->name}} </td>
-                                    <td class="py-4 px-6 border-b border-gray-200">{{$user->email}} </td>
-                                    <td class="py-4 px-6 border-b border-gray-200">{{$user->email_verified_at}} </td>
-                                    <td class="py-4 px-6 border-b border-gray-200">{{$user->created_at}} </td>
-                                    <td class="py-4 px-6 border-b border-gray-200">{{$user->roles}} </td>
-                                    <td class="py-4 px-6 border-b border-gray-200">{{$user->roles}} </td>
-
-                                </tr>
-                                @endforeach
-                            </tbody>
-                    </table>
-                </div>
-                <div class="m-4">
-                {{ $users->links() }}
-                </div>
-            </div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-2">
+                <livewire:users.user-table />
             </div>
         </div>
-    </x-app-layout>
+    </div>
+</x-app-layout>
