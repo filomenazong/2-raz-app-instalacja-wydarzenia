99912911d296444f6bb65630d81ad23d6b46b1b6
filomenazong <filomenazong@gmail.com>
2025-01-02 12:33:24 +0000

    znajduje index.blade wyświetla tabelę

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,9 @@ Route::middleware([
     })->name('dashboard');
 
     // Users routes
-    Route::name('users.')->prefix('users')->group(function () {
-        Route::get('', [UserController::class, 'index'])
-            ->name('users.index')
-            //uprawnienia do logowania
-           // ->middleware(['permission:users.index']);
-            ->middleware(['users.index']);
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
-    });
+    
 
     // Rentedrooms routes - tylko jeden raz
     Route::resource('rentedrooms', RentedroomController::class)->only(['index']);
